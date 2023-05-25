704a9772b15f3a87cedc08e0fac7b17146483ee9
Laya Hamekhani <laya.hamehkhani@gmail.com>
2023-05-25 12:04:33 +0330

    animator

--- animator.py
+++ animator.py
@@ -1,5 +1,6 @@
-from json import load
 from turtle import color
+
+from matplotlib.pyplot import arrow
 from simulator import Simulator
 import tkinter as tk
 from PIL import ImageTk, Image
@@ -12,6 +13,7 @@ class Animator():
         
         
     def setup(self):
+        
         self.path = "D:\Theory\Reinforcement-Learning-Traffic-Light\crossroad.jpg"
         self.load = Image.open(self.path)
         self.load = self.load.resize((475, 475))
@@ -24,85 +26,72 @@ class Animator():
         self.canvas = tk.Canvas(self.window, width=self.load.width, height=self.load.height)
         self.canvas.create_image(0, 0, image=self.img, anchor=tk.NW)
         
-        circle_up = self.canvas.create_oval(160, 10, 300, 150)
-        circle_left = self.canvas.create_oval(10, 160, 150, 300)
-        circle_right = self.canvas.create_oval(310, 160, 450, 300)
-        circle_down = self.canvas.create_oval(160, 310, 300, 460)
-        
-        self.text_right = self.canvas.create_text(380, 230, text=str(self.simulator.cars[0]), font=("Arial", 30))
-        self.text_up = self.canvas.create_text(230, 80, text=str(self.simulator.cars[1]), font=("Arial", 30))
-        self.text_left = self.canvas.create_text(80, 230, text=str(self.simulator.cars[2]), font=("Arial", 30))
-        self.text_down = self.canvas.create_text(230, 380, text=str(self.simulator.cars[3]), font=("Arial", 30))
-        
-        self.text_number_right = self.canvas.create_text(40, 50, text="Right: " + str(self.number), font=("Arial", 10))
-        self.text_number_up = self.canvas.create_text(40, 65, text="Up: " + str(self.number), font=("Arial", 10))
-        self.text_number_left = self.canvas.create_text(40, 80, text="Left: " + str(self.number), font=("Arial", 10))
-        self.text_number_down = self.canvas.create_text(40, 95, text="Down: " + str(self.number), font=("Arial", 10))
-        
-        self.arrow_down = self.canvas.create_line(230, 239, 230, 300, arrow=tk.LAST, width=5, fill="gray")
+        #Creating Circles & texts
+        self.circle = []
+        self.text = []
+        x , m = 0, 0
+        for i in range(2):
+            self.circle.append(self.canvas.create_oval(310 - x, 160 - x, 450 - x, 300 - x))
+            self.circle.append(self.canvas.create_oval(10 + x, 160 + x, 150 + x, 300 + x))
+            self.text.append(self.canvas.create_text(380 - x, 230 - x,text = str(self.simulator.cars[m]), font=("Arial", 30)))
+            self.text.append(self.canvas.create_text(80 + x, 230 + x, text = str(self.simulator.cars[m+2]), font=("Arial", 30)))
+            x = 150
+            m = 1
+            
+        self.direction= ["Right: ", "Up: ", "Left: ", "Down: "]
+        self.text_number = []
+        ini = 50
+        for f in range(4):
+            self.text_number.append(self.canvas.create_text(40, ini ,text = self.direction[f], font=("Arial", 10)))
+            ini += 15
+        self.text_counter = self.canvas.create_text(40, 35, text="counter: " + str(self.simulator.counter), font=("Arial", 10))
+    
+        self.arrow_right = self.canvas.create_line(234, 235, 302, 235, arrow=tk.LAST, width=5, fill="gray")
         self.arrow_up = self.canvas.create_line(230, 231, 230, 160, arrow=tk.LAST, width=5, fill="gray")
         self.arrow_left = self.canvas.create_line(226, 235, 158, 235, arrow=tk.LAST, width=5, fill="gray")
-        self.arrow_right = self.canvas.create_line(234, 235, 302, 235, arrow=tk.LAST, width=5, fill="gray")
-        
-        self.text_counter = self.canvas.create_text(40, 35, text="counter: " + str(self.simulator.counter), font=("Arial", 10))
+        self.arrow_down = self.canvas.create_line(230, 239, 230, 300, arrow=tk.LAST, width=5, fill="gray")
         self.canvas.pack(fill=tk.BOTH, expand=True)
         
     def update(self):
         self.added_cars_ani = [0, 0, 0 , 0]
         for i in range(101):
-            self.sum = 0
-            self.window.after(5000)
+            self.window.after(1000)
             self.window.update()
             self.simulator.update()
             self.added_cars_ani = self.simulator.numbers()
             
             current_dir = self.simulator.current_light
-            self.arrow_down = self.canvas.create_line(230, 239, 230, 300, arrow=tk.LAST, width=5, fill="red")
+            self.arrow_right = self.canvas.create_line(234, 235, 302, 235, arrow=tk.LAST, width=5, fill="red")
             self.arrow_up = self.canvas.create_line(230, 231, 230, 160, arrow=tk.LAST, width=5, fill="red")
             self.arrow_left = self.canvas.create_line(226, 235, 158, 235, arrow=tk.LAST, width=5, fill="red")
-            self.arrow_right = self.canvas.create_line(234, 235, 302, 235, arrow=tk.LAST, width=5, fill="red")
+            self.arrow_down = self.canvas.create_line(230, 239, 230, 300, arrow=tk.LAST, width=5, fill="red")
             
-            self.canvas.itemconfig(self.text_right, text=str(self.simulator.cars[0]))
-            self.canvas.itemconfig(self.text_up, text=str(self.simulator.cars[1]))
-            self.canvas.itemconfig(self.text_left, text=str(self.simulator.cars[2]))
-            self.canvas.itemconfig(self.text_down, text=str(self.simulator.cars[3]))
+            for e in range(4):
+                self.canvas.itemconfig(self.text[e], text=str(self.simulator.cars[e]))
              
             if(current_dir == 0):
-                self.canvas.itemconfig(self.arrow_right, fill = "green")
-                if (self.simulator.cars[0] == 0):
-                    self.canvas.itemconfig(self.text_number_right, text="Right: 0")
-                else:
-                    self.canvas.itemconfig(self.text_number_right, text="Right: " + str(self.added_cars_ani[0] - 10))
-                self.canvas.itemconfig(self.text_number_up, text="Up: " + str(self.added_cars_ani[1]))
-                self.canvas.itemconfig(self.text_number_left, text="Left: " + str(self.added_cars_ani[2]))
-                self.canvas.itemconfig(self.text_number_down, text="Down: " + str(self.added_cars_ani[3]))
+                self.arrow_right = self.canvas.itemconfig(self.arrow_right, fill="green")
+                self.canvas.itemconfig(self.text_number[0], text="\tRight: " + str(self.added_cars_ani[0]) + " decrese: 10")
+                for q in range(1, 4):
+                    self.canvas.itemconfig(self.text_number[q], text=self.direction[q] + str(self.added_cars_ani[q]))
             elif(current_dir == 1):
                 self.canvas.itemconfig(self.arrow_up, fill = "green")
-                self.canvas.itemconfig(self.text_number_right, text="Right: " + str(self.added_cars_ani[0]))
-                if (self.simulator.cars[1] == 0):
-                    self.canvas.itemconfig(self.text_number_up, text="Up: 0")
-                else:
-                    self.canvas.itemconfig(self.text_number_up, text="Up: " + str(self.added_cars_ani[1] - 10))
-                self.canvas.itemconfig(self.text_number_left, text="Left: " + str(self.added_cars_ani[2]))
-                self.canvas.itemconfig(self.text_number_down, text="Down: " + str(self.added_cars_ani[3]))
+                self.canvas.itemconfig(self.text_number[0], text="Right: " + str(self.added_cars_ani[0]))
+                self.canvas.itemconfig(self.text_number[1], text="\tUp: " + str(self.added_cars_ani[1]) + " decrese: 10")
+                self.canvas.itemconfig(self.text_number[2], text="Left: " + str(self.added_cars_ani[2]))
+                self.canvas.itemconfig(self.text_number[3], text="Down: " + str(self.added_cars_ani[3]))
             elif(current_dir == 2):
                 self.canvas.itemconfig(self.arrow_left, fill = "green")
-                self.canvas.itemconfig(self.text_number_right, text="Right: " + str(self.added_cars_ani[0]))
-                self.canvas.itemconfig(self.text_number_up, text="Up: " + str(self.added_cars_ani[1]))
-                if (self.simulator.cars[2] == 0):
-                    self.canvas.itemconfig(self.text_number_left, text="Left: 0")
-                else:
-                    self.canvas.itemconfig(self.text_number_left, text="Left: " + str(self.added_cars_ani[2]) - 10)
-                self.canvas.itemconfig(self.text_number_down, text="Down: " + str(self.added_cars_ani[3]))
+                self.canvas.itemconfig(self.text_number[0], text="Right: " + str(self.added_cars_ani[0]))
+                self.canvas.itemconfig(self.text_number[1], text="Up: " + str(self.added_cars_ani[1]))
+                self.canvas.itemconfig(self.text_number[2], text="\tLeft: " + str(self.added_cars_ani[2]) + " decrese: 10")
+                self.canvas.itemconfig(self.text_number[3], text="Down: " + str(self.added_cars_ani[3]))
             elif(current_dir == 3):
                 self.canvas.itemconfig(self.arrow_down, fill = "green")
-                self.canvas.itemconfig(self.text_number_right, text="Right: " + str(self.added_cars_ani[0]))
-                self.canvas.itemconfig(self.text_number_up, text="Up: " + str(self.added_cars_ani[1]))
-                self.canvas.itemconfig(self.text_number_left, text="Left: " + str(self.added_cars_ani[2]))
-                if (self.simulator.cars[3] == 0):
-                    self.canvas.itemconfig(self.text_number_down, text="Down: 0")
-                else:    
-                    self.canvas.itemconfig(self.text_number_down, text="Down: " + str(self.added_cars_ani[3]) - 10)
+                self.canvas.itemconfig(self.text_number[0], text="Right: " + str(self.added_cars_ani[0]))
+                self.canvas.itemconfig(self.text_number[1], text="Up: " + str(self.added_cars_ani[1]))
+                self.canvas.itemconfig(self.text_number[2], text="Left: " + str(self.added_cars_ani[2]))
+                self.canvas.itemconfig(self.text_number[3], text="\tDown: " + str(self.added_cars_ani[3]) + " decrese: 10")
             self.canvas.itemconfig(self.text_counter, text="counter: " + str(self.simulator.counter))
             
         self.window.mainloop()
